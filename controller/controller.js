ac1e8286cb4260644287163137ebab86feb9cd87
smreinar <stevenreinart123@gmail.com>
2020-06-10 11:56:52 -0700

    fixed /scrape to grab all articles

--- controller/controller.js
+++ controller/controller.js
@@ -17,8 +17,8 @@ router.get("/scrape", function(req, res){
     axios.get("https://sciworthy.com/?gclid=Cj0KCQjwiYL3BRDVARIsAF9E4GdTweUqCuwywMtMZA-vJYvoU0TLFNiyqVod8ictSfcUP2buBHnyBAoaAhDsEALw_wcB").then(function(res){
         var $ = cheerio.load(res.data);
         var titlesArray = [];
-        $("div.fg-item").each(function(i, element){
-            var title =$(element).find("h2").text();
+        $("article").each(function(i, element){
+            var title =$(element).find("span.entry-title-primary").text();
             var link = $(element).children().attr("href");
             titlesArray.push({
                 title: title,
